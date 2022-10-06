723596fda5c14b9684ddc5df7c08eb6b1a20d54a
Justin Tran <justinsang15@gmail.com>
2022-10-06 12:40:54 +0700

    feat: Cart & overlay background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
 
@@ -6,7 +7,10 @@ const App = () => {
   return (
     <>
       <Header />
-      <Meals />
+      <main>
+        <Meals />
+        {/* <Cart /> */}
+      </main>
     </>
   );
 };
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styles from "./Modal.module.css";
+
+const Overlay = (props) => {
+  return (
+    <>
+      <div className={styles.backdrop}></div>
+      <div className={styles.modal}>
+        <div>{props.children}</div>
+      </div>
+    </>
+  );
+};
+
+const Modal = (props) =>
+  ReactDOM.createPortal(
+    <Overlay>{props.children}</Overlay>,
+    document.querySelector("#overlay")
+  );
+export default Modal;
