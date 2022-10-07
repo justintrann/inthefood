0008be323d81e27ca6d04f0397daf0d73c765abd
Justin Tran <justinsang15@gmail.com>
2022-10-08 00:25:33 +0700

    feat: show item cart added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,50 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import CartContext from "../../storing/cart-context";
 import Modal from "../UI/Modal/Modal";
 import styles from "./Cart.module.css";
+
+import CartItem from "./CartItem/CartItem";
 const Cart = (props) => {
   // Get Data
   const cartData = useContext(CartContext);
   // func
+  const onAddHandler = (items) => {};
+  const onRemoveHandler = (id) => {};
+
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", qty: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+      {cartData.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onAdd={onAddHandler.bind(null, item)}
+          onRemove={onRemoveHandler.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
-
+  useEffect(() => {
+    console.log(cartData);
+    return () => {
+      console.log("Unmount");
+    };
+  });
   return (
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
         <span>Total: </span>
-        <span>{cartData.totalAmount}</span>
+        <span>{cartData.totalAmount.toFixed(2)}</span>
       </div>
       <div className={styles.actions}>
         <button onClick={props.onHideCart} className={styles["button--alt"]}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {cartData.items.length > 0 && (
+          <button className={styles.button}>Order</button>
+        )}
       </div>
     </Modal>
   );
