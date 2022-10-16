1ee9ddb3c3ef2e03b7109a57c5723b8e95350a89
Justin Tran <justinsang15@gmail.com>
2022-10-16 20:57:39 +0700

    feat: get meal data through Axios

--- src/components/Meals/Available-Meal/AvailableMeals.js
+++ src/components/Meals/Available-Meal/AvailableMeals.js
@@ -1,37 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../UI/Card/Card";
 import MealItem from "../Meal-item/MealItem";
 import styles from "./AvailableMeals.module.css";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { API_URL } from "../../../helper/helperUrl";
+import axios from "axios";
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
 
 const AvailableMeals = () => {
-  const mealLists = DUMMY_MEALS.map((meal) => (
+  const [dataMeals, setDataMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(1);
+
+  useEffect(() => {
+    axios.get(API_URL).then((res) => {
+      const data = Object.keys(res.data).map((key) => {
+        return { id: key, ...res.data[key] };
+      });
+      setDataMeals(data);
+      setIsLoading(0);
+    });
+  }, []);
+
+  if (isLoading) return <p className={styles.loading}>Loading ....</p>;
+
+  const mealLists = dataMeals.map((meal) => (
     <MealItem key={meal.id} {...meal} />
   ));
 
---
+++ src/helper/helperUrl.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  "https://react-http-64061-default-rtdb.asia-southeast1.firebasedatabase.app/receipts.json";
